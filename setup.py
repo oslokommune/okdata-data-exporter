e75f1c77d2637808981db5a09334833d2be8ae50
Simen Heggestøyl <simen.heggestoyl@origo.oslo.kommune.no>
2021-05-18 10:46:30 +0200

    Rename repository to `okdata-data-exporter`

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 
 setuptools.setup(
-    name="data-exporter",
+    name="okdata-data-exporter",
     version="0.0.1",
     author="Origo Dataplattform",
     author_email="dataplattform@oslo.kommune.no",
     description="Lambda function to export data",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/oslokommune/data-exporter",
+    url="https://github.com/oslokommune/okdata-data-exporter",
     packages=setuptools.find_packages(),
     install_requires=[
         "aws-xray-sdk",
